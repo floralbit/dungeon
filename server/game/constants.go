594c2841b6a9382ec2eb9cdeb7df7758129f6a90
floralbit <2bitghost@unsafe.host>
2021-02-08 18:30:27 -0800

    small fix

--- server/game/constants.go
+++ server/game/constants.go
@@ -9,8 +9,7 @@ var dungeonFloor1UUID = uuid.MustParse("6a67086c-eb9c-44c1-85b1-a140df7e4272")
 var overworldSpawnObjectUUID = uuid.MustParse("b4f195f7-644a-4791-8177-c9eb69b10e9e")
 var dungeonEntranceObjectUUID = uuid.MustParse("85ab1aaf-fcb2-4fa2-80e0-3cf54f8cad41")
 
-var gameTileset = loadTiledTileset("../data/tileset.json")
-var tiles = convertTileset(gameTileset)
+var tiles = convertTileset(loadTiledTileset("../data/tileset.json"))
 var monsterTemplates = loadMonsterTemplates()
 
 var zones = loadZones()
